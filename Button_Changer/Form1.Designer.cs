ed96bc4edf9ca217388e8ad0649d01e95e9c3149
Teo <64589970+953rd@users.noreply.github.com>
2023-03-02 01:19:35 +0300

    add project

---
+++ Button_Changer/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿
+namespace Button_Changer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::Button_Changer.Properties.Resources.condition;
+            this.pictureBox1.Location = new System.Drawing.Point(90, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(621, 41);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(12, 117);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 445);
+            this.panel1.TabIndex = 1;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(391, 225);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(165, 58);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Кнопка №4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(220, 225);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(165, 58);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Кнопка №3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(391, 161);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(165, 58);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Кнопка №2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(220, 161);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 58);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Кнопка №1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 574);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Button_Changer";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Button_Changer/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Button_Changer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Size = new Size(100, 35);
+            button2.Size = new Size(150, 70);
+            button3.Size = new Size(150, 70);
+            button4.Size = new Size(150, 70);
+            button1.Location = new Point(0, 0);
+            button2.Location = new Point(624, 0);
+            button3.Location = new Point(0, 293);
+            button4.Location = new Point(624, 293);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.Size = new Size(200, 100);
+            button2.Size = new Size(100, 35);
+            button3.Size = new Size(200, 100);
+            button4.Size = new Size(200, 100);
+            button1.Location = new Point(287, 0);
+            button2.Location = new Point(674, 171);
+            button3.Location = new Point(287, 343);
+            button4.Location = new Point(0, 171);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button1.Size = new Size(250, 150);
+            button2.Size = new Size(250, 150);
+            button3.Size = new Size(100, 35);
+            button4.Size = new Size(250, 150);
+            button1.Location = new Point(410, 66);
+            button2.Location = new Point(110, 66);
+            button3.Location = new Point(167, 238);
+            button4.Location = new Point(410, 238);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            button1.Size = new Size(300, 200);
+            button2.Size = new Size(300, 200);
+            button3.Size = new Size(300, 200);
+            button4.Size = new Size(100, 35);
+            button1.Location = new Point(65, 230);
+            button2.Location = new Point(415, 15);
+            button3.Location = new Point(65, 15);
+            button4.Location = new Point(415, 230);
+        }
+    }
+}
